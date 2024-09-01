77590b2046ccecae42092d84894234075d09914e
Jeeyong Um <conr2d@proton.me>
2024-09-01 23:47:07 +0900

    Reformat

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -6,15 +6,15 @@ import { filterPosts, importPosts } from '$lib/data/blog-posts/utils';
 export const prerender = true;
 
 export async function GET() {
-  const allPosts = importPosts(true);
-  const filteredPosts = filterPosts(allPosts);
+	const allPosts = importPosts(true);
+	const filteredPosts = filterPosts(allPosts);
 
-  const body = xml(filteredPosts);
-  const headers = {
-    'Cache-Control': 'max-age=0, s-maxage=3600',
-    'Content-Type': 'application/xml'
-  };
-  return new Response(body, { headers });
+	const body = xml(filteredPosts);
+	const headers = {
+		'Cache-Control': 'max-age=0, s-maxage=3600',
+		'Content-Type': 'application/xml'
+	};
+	return new Response(body, { headers });
 }
 
 const xml = (posts: BlogPost[]) => `
@@ -41,8 +41,8 @@ const xml = (posts: BlogPost[]) => `
       <height>32</height>
     </image>
     ${posts
-    .map(
-      (post) => `
+			.map(
+				(post) => `
         <item>
           <guid>${siteBaseUrl}/${post.slug}</guid>
           <title>${post.title}</title>
@@ -66,7 +66,7 @@ const xml = (posts: BlogPost[]) => `
           ${post.coverImage ? `<media:content xmlns:media="http://search.yahoo.com/mrss/" medium="image" url="${siteBaseUrl}/${post.coverImage}"/>` : ''}          
         </item>
       `
-    )
-    .join('')}
+			)
+			.join('')}
   </channel>
 </rss>`;
